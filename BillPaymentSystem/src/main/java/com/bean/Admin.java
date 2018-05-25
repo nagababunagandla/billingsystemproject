982706e21130b44f957954967944a40d1fedab34
nagababunagandla <39617554+nagababunagandla@users.noreply.github.com>
2018-05-25 15:03:10 +0530

    Add files via upload

---
+++ BillPaymentSystem/src/main/java/com/bean/Admin.java
@@ -0,0 +1,34 @@
+package com.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admin")
+public class Admin {
+	
+	@Id
+	private int id;
+	private String username;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
+
